76c524500fd61f9b041e7c61e1139a7f1e6d1bdb
root <root@s67089.dedi.leaseweb.net>
2025-08-05 17:49:24 +0000

    commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,4 +13,4 @@ websockify -D --web=/usr/share/novnc/ localhost:$NO_VNC_PORT localhost:$VNC_PORT
 vncserver $DISPLAY -depth $VNC_COL_DEPTH -geometry $VNC_RESOLUTION -SecurityTypes None -localhost no --I-KNOW-THIS-IS-INSECURE &
 nginx -c /etc/nginx/nginx.conf &
 wait 10
-firefox --profile /root/profile --new-tab https://$VPN_IP &
+firefox &
